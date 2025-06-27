45ff9da74cf22a50b9bb1394fe08d349cf96cc1d
Ramses Larabel <rlarabel@gmail.com>
2025-06-27 02:49:05 +0000

    build(format): integrate clang format
    Update clang-format file. Add clang-format to docker set-up.
    Add a format check to ci.yml. Insert format rule in Makefile.

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 int main(void)
 {
     volatile unsigned int i;
-    WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-    P1DIR |= 0x01;                            // Set P1.0 to output direction
+    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+    P1DIR |= 0x01; // Set P1.0 to output direction
 
-    while(1)
-    {
-        P1OUT ^= 0x01;                       // Toggle P1.0 using exclusive-OR
+    while (1) {
+        P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
 
-        for (i=20000; i>0; i--);
-  }
+        for (i = 20000; i > 0; i--)
+            ;
+    }
 }
