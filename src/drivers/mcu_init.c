32aa563935212e56b19cf824f4ff730b2a510d55
Ramses Larabel <rlarabel@gmail.com>
2025-12-10 20:35:14 -0500

    feat(drivers): Implement IO Handling
    Create a driver to handle initializing the mcu and any pheripherials (e.g watchdog, clocks, etc).
    Implement a UI to abstract and simplify the TI's library that handles the hardware.
    Map the enums to actual pins on the board, add logic to configure and read each pin.
    Test the firmware using test functions to use each pin as an output and input.

---
+++ src/drivers/mcu_init.c
@@ -0,0 +1,17 @@
+#include "mcu_init.h"
+#include "io.h"
+#include <msp430.h>
+
+/* Watchdog is set by default
+ * and repeatedly reset mcu if not turned off.
+ */
+static void watchdog_stop(void)
+{
+    WDTCTL = WDTPW + WDTHOLD;
+}
+
+void mcu_init(void)
+{
+    // Must stop watchdog first
+    watchdog_stop();
+}
---
+++ src/drivers/mcu_init.h
@@ -0,0 +1,10 @@
+#ifndef MCU_INIT_H
+#define MCU_INIT_H
+
+/* Initialize common mcu functionality that affect all
+ * peripherals (e.g. watchdog, clocks, pins)
+ */
+
+void mcu_init(void);
+
+#endif
