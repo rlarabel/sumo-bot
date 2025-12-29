0913c0df6377502a6508c16e2c29b4f2108ac637
Ramses Larabel <rlarabel@gmail.com>
2025-12-29 14:07:23 -0500

    fix(init): Increase Clock Speed
    Increase clock speed to increase reaction time for robot. Does use
    more power, but is nothing to worry about for the sumo bot. Update
    BUSY_WAIT(ms) function to operate correctly with the faster clock.

--- src/drivers/mcu_init.c
+++ src/drivers/mcu_init.c
@@ -1,6 +1,21 @@
 #include "mcu_init.h"
 #include "io.h"
+#include "../common/assert_handler.h"
 #include <msp430.h>
+
+static void init_clocks(void)
+{
+    // Check to make sure calibration data has not been erased
+    ASSERT(CALBC1_1MHZ != 0xFF && CALBC1_16MHZ != 0xFF);
+
+    /* Configure internal oscillator to run at 16 MHz
+     * Used as a reference to produce a more stable DCO
+     */
+    BCSCTL1 |= CALBC1_16MHZ;
+
+    // Sets the clock rate of the digitally controlled oscillator (DCO)
+    DCOCTL |= CALDCO_16MHZ;
+}
 
 /* Watchdog is set by default
  * and repeatedly reset mcu if not turned off.
@@ -15,6 +30,7 @@ void mcu_init(void)
     // Must stop watchdog first
     watchdog_stop();
     io_init();
+    init_clocks();
     // Enables globaly
     _enable_interrupts();
 }
